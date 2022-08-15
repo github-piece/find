5f81b5addb99835dd885216843139ecdad567fb3
excellent-coding <dw951012@gmail.com>
2022-08-15 17:08:31 +0200

    radix label import

--- apps/web-app/src/components/radix/Input.tsx
+++ apps/web-app/src/components/radix/Input.tsx
@@ -1,20 +1,47 @@
-import * as LabelPrimitive from '@radix-ui/react-label';
+import classNames from 'classnames';
+import Label from './Label';
 
 type InputProps = {
   label?: string
   placeholder?: string
+  type?: string
+  error?: string
+  value: string
+  onChange: (v: string) => void
+  className?: string
+  inputClassName?: string
+  labelClassName?: string
+  errorClassName?: string
+  onKeyUp?: (e: React.KeyboardEvent<HTMLInputElement>) => void
 }
 
-const Input: React.FC<InputProps> = ({ label, placeholder = "" }) => (
-  <div className="">
-    {!!label && <label className="block text-gray-700 text-sm font-bold mb-2">
-      {label}
-    </label>}
+const Input: React.FC<InputProps> = ({
+  label,
+  type,
+  placeholder = "",
+  value,
+  onChange,
+  onKeyUp,
+  className = "",
+  inputClassName = "",
+  labelClassName = "",
+  error,
+  errorClassName = ""
+}) => (
+  <div className={className}>
+    {!!label && <Label text={label} className={labelClassName} />}
     <input
-      type="string"
-      className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-      placeholder={placeholder} />
-    <p className="text-red-500 text-xs italic">Please choose a password.</p>
+      type={type || "string"}
+      className={classNames(
+        "form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
+        inputClassName
+      )}
+      value={value}
+      onChange={e => onChange(e.target.value)}
+      onKeyUp={onKeyUp}
+      placeholder={placeholder}
+    />
+    {error && <p className={classNames("text-red-500 text-xs italic", errorClassName)}>{error}</p>}
   </div>
 )
 
