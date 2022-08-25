33fff186a77a33ec9ec29e5781c0328f1455b7e3
Khalil Omer <68179593+khalil-omer@users.noreply.github.com>
2022-08-25 14:33:51 -0700

    Update UI text

--- apps/web-app/src/pages/auth/enter-password.tsx
+++ apps/web-app/src/pages/auth/enter-password.tsx
@@ -1,28 +1,27 @@
-import { FormEvent, useEffect, useState } from "react"
-import Button from "../../components/Button"
-import EyeIcon from "../../assets/icon/eye.svg"
+import { FormEvent, useEffect, useState } from "react";
+import Button from "../../components/Button";
+import EyeIcon from "../../assets/icon/eye.svg";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Input from "../../components/radix/Input";
 
 const EnterPassword = () => {
-  const { status } = useSession()
-  const router = useRouter()
-  const [password, setPassword] = useState('');
+  const { status } = useSession();
+  const router = useRouter();
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [type, setType] = useState('password')
+  const [type, setType] = useState("password");
 
-  const handleType = () => setType(type === 'text' ? 'password' : 'text')
+  const handleType = () => setType(type === "text" ? "password" : "text");
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   useEffect(() => {
-    if (status === 'unauthenticated') 
-      router.push('/login')
-  }, [status, router])
+    if (status === "unauthenticated") router.push("/login");
+  }, [status, router]);
 
-  if (status !== 'authenticated') return <></>
+  if (status !== "authenticated") return <></>;
 
   return (
     <div className="max-w-lg mx-auto w-full">
@@ -30,7 +29,8 @@ const EnterPassword = () => {
         Enter your Find master password
       </h1>
       <p className="text-gray-400 text-sm mb-12 font-semibold mb-8">
-        Your private data in Find is end-to-end-encrypted. Enter the master password for your account to unlock.
+        Your private data in Find is end-to-end-encrypted. Enter the master
+        password for your account to unlock.
       </p>
       <form onSubmit={handleSubmit}>
         <Input
@@ -45,7 +45,7 @@ const EnterPassword = () => {
         />
         <Button
           type="submit"
-          text="Log in"
+          text="Unlock"
           solid
           full
           className="mx-0"
@@ -53,9 +53,9 @@ const EnterPassword = () => {
         />
       </form>
     </div>
-  )
-}
+  );
+};
 
-EnterPassword.layout = "Auth"
+EnterPassword.layout = "Auth";
 
-export default EnterPassword
+export default EnterPassword;
