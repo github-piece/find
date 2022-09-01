11d2bce9fcaf8a2b5ff12774ac4e269ca205c3c1
excellent-coding <dw951012@gmail.com>
2022-08-31 20:34:36 -0700

    select component & home page & enter password

--- apps/web-app/src/components/radix/Select.tsx
+++ apps/web-app/src/components/radix/Select.tsx
@@ -6,80 +6,74 @@ import {
 import * as SelectPrimitive from "@radix-ui/react-select";
 import classNames from "classnames";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 import { inputBaseClass } from "./Input";
 import Label from "./Label";
 
 type SelectProps = {
   label?: string;
   placeholder?: string;
-  type?: string;
-  error?: string;
-  value: string;
+  value?: string;
   onChange: (v: string) => void;
+  options: { value: string; label: string }[];
   className?: string;
   inputClassName?: string;
   labelClassName?: string;
-  errorClassName?: string;
-  onKeyUp?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  icon?: any;
-  onIconClick?: () => void;
 };
 
-const Select = ({ className = "", placeholder = "" }) => {
+const Select: React.FC<SelectProps> = ({
+  label,
+  className = "",
+  placeholder = "",
+  inputClassName = "",
+  labelClassName = "",
+  value = "",
+  onChange,
+  options = [],
+}) => {
   const { theme } = useTheme();
   return (
-    <SelectPrimitive.Root>
-      <SelectPrimitive.Trigger
-        className={classNames(inputBaseClass, className, theme)}
-        aria-label="Food"
-      >
-        <SelectPrimitive.Value placeholder={placeholder} />
-        <SelectPrimitive.Icon className="ml-auto my-auto">
-          <ChevronDownIcon />
-        </SelectPrimitive.Icon>
-      </SelectPrimitive.Trigger>
-      <SelectPrimitive.Content className="inline-flex items-center bg-white px-0 py-4 gap-2">
-        <SelectPrimitive.ScrollUpButton>
-          <ChevronUpIcon />
-        </SelectPrimitive.ScrollUpButton>
-        <SelectPrimitive.Viewport>
-          <SelectPrimitive.Item value="apple">
-            <SelectPrimitive.ItemText>Apple</SelectPrimitive.ItemText>
-            <SelectPrimitive.ItemIndicator>
-              <CheckIcon />
-            </SelectPrimitive.ItemIndicator>
-          </SelectPrimitive.Item>
-          <SelectPrimitive.Item value="banana">
-            <SelectPrimitive.ItemText>Banana</SelectPrimitive.ItemText>
-            <SelectPrimitive.ItemIndicator>
-              <CheckIcon />
-            </SelectPrimitive.ItemIndicator>
-          </SelectPrimitive.Item>
-          <SelectPrimitive.Item value="blueberry">
-            <SelectPrimitive.ItemText>Blueberry</SelectPrimitive.ItemText>
-            <SelectPrimitive.ItemIndicator>
-              <CheckIcon />
-            </SelectPrimitive.ItemIndicator>
-          </SelectPrimitive.Item>
-          <SelectPrimitive.Item value="grapes">
-            <SelectPrimitive.ItemText>Grapes</SelectPrimitive.ItemText>
-            <SelectPrimitive.ItemIndicator>
-              <CheckIcon />
-            </SelectPrimitive.ItemIndicator>
-          </SelectPrimitive.Item>
-          <SelectPrimitive.Item value="pineapple">
-            <SelectPrimitive.ItemText>Pineapple</SelectPrimitive.ItemText>
-            <SelectPrimitive.ItemIndicator>
-              <CheckIcon />
-            </SelectPrimitive.ItemIndicator>
-          </SelectPrimitive.Item>
-        </SelectPrimitive.Viewport>
-        <SelectPrimitive.ScrollDownButton>
-          <ChevronDownIcon />
-        </SelectPrimitive.ScrollDownButton>
-      </SelectPrimitive.Content>
-    </SelectPrimitive.Root>
+    <div className={classNames(className, theme)}>
+      {!!label && (
+        <Label
+          text={label}
+          className={classNames("text-left", labelClassName)}
+        />
+      )}
+      <SelectPrimitive.Root onValueChange={onChange} defaultValue={value}>
+        <SelectPrimitive.Trigger
+          className={classNames(inputBaseClass, inputClassName)}
+        >
+          <SelectPrimitive.Value placeholder={placeholder} />
+          <SelectPrimitive.Icon className="ml-auto my-auto">
+            <ChevronDownIcon />
+          </SelectPrimitive.Icon>
+        </SelectPrimitive.Trigger>
+        <SelectPrimitive.Portal>
+          <SelectPrimitive.Content className="bg-white py-4 px-0 gap-2 shadow z-50 rounded-lg w-fit mx-auto">
+            <SelectPrimitive.ScrollUpButton>
+              <ChevronUpIcon />
+            </SelectPrimitive.ScrollUpButton>
+            <SelectPrimitive.Viewport className="p-1">
+              {options.map(({ value, label }) => (
+                <SelectPrimitive.Item
+                  key={value}
+                  value={value}
+                  className="flex items-center relative rounded-sm px-12 hover:bg-gray-100 w-full outline-0"
+                >
+                  <SelectPrimitive.ItemText>{label}</SelectPrimitive.ItemText>
+                  <SelectPrimitive.ItemIndicator className="absolute w-6 items-center justify-center left-0">
+                    <CheckIcon />
+                  </SelectPrimitive.ItemIndicator>
+                </SelectPrimitive.Item>
+              ))}
+            </SelectPrimitive.Viewport>
+            <SelectPrimitive.ScrollDownButton>
+              <ChevronDownIcon />
+            </SelectPrimitive.ScrollDownButton>
+          </SelectPrimitive.Content>
+        </SelectPrimitive.Portal>
+      </SelectPrimitive.Root>
+    </div>
   );
 };
 
