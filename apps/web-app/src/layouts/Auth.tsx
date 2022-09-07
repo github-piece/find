cd8cbec1fbc74088dbe6441e5e4f594bc0f81f0d
excellent-coding <dw951012@gmail.com>
2022-09-07 16:47:56 -0700

    update theme as resolved

--- apps/web-app/src/layouts/Auth.tsx
+++ apps/web-app/src/layouts/Auth.tsx
@@ -1,12 +1,12 @@
-import classNames from "classnames";
-import { useTheme } from "next-themes";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import classNames from 'classnames';
+import { useTheme } from 'next-themes';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
   return (
-    <div className={theme}>
+    <div className={resolvedTheme}>
       <div className="bg-white dark:bg-dark">
         <Navbar />
         <div className="flex mx-auto w-full text-center lg:h-[calc(100vh-160px)] items-center">
--- apps/web-app/src/layouts/Basic.tsx
+++ apps/web-app/src/layouts/Basic.tsx
@@ -1,22 +1,22 @@
-import { useSession } from "next-auth/react";
-import { useTheme } from "next-themes";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useSession } from 'next-auth/react';
+import { useTheme } from 'next-themes';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const BasicLayout = ({ children }: { children: React.ReactNode }) => {
   const { status } = useSession();
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
   const router = useRouter();
   useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/login");
+    if (status === 'unauthenticated') {
+      router.push('/login');
     }
   }, [status, router]);
 
-  if (status !== "authenticated") return <></>;
+  if (status !== 'authenticated') return <></>;
 
   return (
-    <div className={theme}>
+    <div className={resolvedTheme}>
       <div className="bg-white dark:bg-dark">{children}</div>
     </div>
   );
