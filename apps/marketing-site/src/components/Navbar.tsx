e0e2456b866d02180f85c6204cc3173fc783fe83
Excellent-coding <55793602+Excellent-coding@users.noreply.github.com>
2022-10-26 22:42:25 -0400

    Update Navbar.tsx

--- apps/marketing-site/src/components/Navbar.tsx
+++ apps/marketing-site/src/components/Navbar.tsx
@@ -67,14 +67,13 @@ const Navbar = () => {
             />
           </Link>
         </div>
-        <div className="flex justify-end -mt-10 mr-8">
+        <Link href='https://find.new/login' className="flex justify-end -mt-10 mr-8">
           <button
-            onClick={handleAuth}
             className="bg-white dark:bg-gray-200-dark hover:bg-gray-400 dark:text-gray-700-dark font-semibold px-4 py-2.5 rounded-xl my-auto"
           >
             {auth}
           </button>
-        </div>
+        </Link>
       </nav>
     );
 };
