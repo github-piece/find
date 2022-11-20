d4197c67cd37fbc727427d2267acc26e0a2d120c
Excellent-coding <55793602+Excellent-coding@users.noreply.github.com>
2022-11-20 08:39:02 +0800

    Update Navbar.tsx

--- apps/marketing-site/src/components/Navbar.tsx
+++ apps/marketing-site/src/components/Navbar.tsx
@@ -44,92 +44,92 @@ const Navbar = () => {
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/getting-started.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Getting Started</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Getting Started</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/browser-extension.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Browser Extension</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Browser Extension</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/generated-results.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Generated Results</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Generated Results</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/api-reference.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">API Reference</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">API Reference</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/privacy-and-security.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Privacy and Security</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Privacy and Security</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/perspectives.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Perspectives</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Perspectives</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/sessions.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Sessions</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Sessions</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/enabling-your-app-for-find-search.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Enabling Your App for Find Search</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Enabling Your App for Find Search</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/billing-and-subscriptions.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Billing and Subscriptions</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Billing and Subscriptions</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/searching-connected-apps.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Searching Connected Apps</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Searching Connected Apps</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/briefings-and-alerts.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Briefings and Alerts</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Briefings and Alerts</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/contributing-to-find.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Contributing to Find</div>
-                    <div className="text-xs text-[#757685]">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Contributing to Find</div>
+                    <div className="text-xs text-[#757685] cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
                 <div className='flex flex-row hover:bg-[#f8f9fd] dark:hover:bg-[#383838] pl-3 rounded-xl'>
                   <Image src={'/assets/searching.svg'} width={24} height={24} alt='' />
                   <span className='m-3'>
-                    <div className="text-[#151515] dark:text-white">Searching</div>
-                    <div className="text-xs text-[#757685] dark:">Learn more about app</div>
+                    <div className="text-[#151515] dark:text-white cursor-pointer">Searching</div>
+                    <div className="text-xs text-[#757685]  cursor-pointer">Learn more about app</div>
                   </span>
                 </div>
               </div>
